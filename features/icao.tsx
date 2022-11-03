15d5677e9c45f9590ddfde6db50d098469bb7e81
kerkness <ryan@kerkness.ca>
2022-11-02 22:17:25 -0500

    codes

---
+++ features/icao.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import codes from './airports.json';
+
+const AirportCodes = () => {
+
+    return (
+        <div>ICAO COdes</div>
+    )
+
+}
+
+export default AirportCodes;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import AirportCodes from '../features/icao'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -22,6 +23,10 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
+
+          <AirportCodes />
+
+
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
